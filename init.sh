85bebab987add93a2168a721cabbf904aeb42d9c
André Videla <andre.videla@gmail.com>
2018-04-27 22:30:14 +0200

    Add mkdir vimFiles

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@
 BASEDIR=~/dotfiles/
 ZSHDIR=~/.zprezto/runcoms/
 
+mkdir ~/.vimFiles/backup
+mkdir ~/.vimFiles/swapfiles
 ln -s $BASEDIR/vimrc ~/.vimrc
 ln -s $BASEDIR/ackrc ~/.ackrc
 ln -s $ZSHDIR/zpreztorc ~/.zpreztorc
