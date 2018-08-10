8d2a2c10f4d00d76714731c2e7c02cf0cbe95111
André Videla <andre.videla@gmail.com>
2018-08-10 12:16:56 +0200

    Add Pathogen

--- init.sh
+++ init.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-BASEDIR=~/dotfiles/
-ZSHDIR=~/.zprezto/runcoms/
 
-mkdir ~/.vimFiles/backup
-mkdir ~/.vimFiles/swapfiles
+BASEDIR=~/dotfiles
+ZSHDIR=~/.zprezto/runcoms
+ 
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+mkdir -p ~/.vimFiles/backup
+mkdir -p ~/.vimFiles/swapfiles
+mv -f ~/.vimrc ~/.vimrc.backup
+mv -f ~/.ackrc ~/.ackrc.backup
 ln -s $BASEDIR/vimrc ~/.vimrc
 ln -s $BASEDIR/ackrc ~/.ackrc
-ln -s $ZSHDIR/zpreztorc ~/.zpreztorc
-ln -s $ZSHDIR/zprofile ~/.zprofile
-ln -s $ZSHDIR/zshrc ~/.zshrc
+rm -rf ~/.zpreztorc
+rm -rf ~/.zprofile
+rm -rf ~/.zshrc
+ln -s $BASEDIR/zpreztorc ~/.zpreztorc
+ln -s $BASEDIR/zprofile ~/.zprofile
+ln -s $BASEDIR/zshrc ~/.zshrc
